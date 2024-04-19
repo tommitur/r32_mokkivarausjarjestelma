520ec77df3dfbac2d7f2f343faf583c5ccaa4b75
saarinik <saarinik@student.uef.fi>
2024-04-19 11:51:48 +0300

    testi22

--- src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
+++ src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
@@ -3,6 +3,7 @@ package ohjtuotanto.varausjarjestelma;
 public class testicommit {
     public static void main(String[] args) {
         System.out.println("testi");
+        System.out.println("testi2");
     }
 
 }
