48c2184668cacc833166ab824a56887dc14bce44
Joskari <jortikka.oskari@gmail.com>
2024-04-19 11:40:53 +0300

    parenpi testi

--- src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
+++ src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
@@ -2,7 +2,7 @@ package ohjtuotanto.varausjarjestelma;
 
 public class testicommit {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("testi");
     }
 
 }
