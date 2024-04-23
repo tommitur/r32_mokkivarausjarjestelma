7aaf798a5bc9fd6b5cae799641dd2c6092830267
tommitur <tombasteri@gmail.com>
2024-04-23 11:44:44 +0300

    mysql connection testi

--- src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
+++ src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
@@ -1,9 +1,31 @@
 package ohjtuotanto.varausjarjestelma;
 
+import java.sql.*;
+
 public class testicommit {
     public static void main(String[] args) {
         System.out.println("testi");
         System.out.println("testi2");
+
+        try{
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/vn",
+                    "root",
+                    "Yksitoista123"
+            );
+
+            Statement statement = connection.createStatement();
+            ResultSet set = statement.executeQuery("select * from alue");
+
+            while(set.next()){
+                System.out.println(set.getInt("alue_id"));
+                System.out.println(set.getString("nimi"));
+            }
+        }catch(SQLException e){
+            System.out.println(e.getErrorCode());
+        }
+
+
     }
 
 }
