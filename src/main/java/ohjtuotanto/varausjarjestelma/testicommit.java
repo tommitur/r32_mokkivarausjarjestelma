36820c27e5f93e7dd8fa7febdd95c01413f8e008
Joskari <jortikka.oskari@gmail.com>
2024-04-24 14:34:05 +0300

    paaohjelman aloittelua ja lisäsin sqlkomennot luokan

--- src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
+++ src/main/java/ohjtuotanto/varausjarjestelma/testicommit.java
@@ -4,14 +4,12 @@ import java.sql.*;
 
 public class testicommit {
     public static void main(String[] args) {
-        System.out.println("testi");
-        System.out.println("testi2");
 
         try{
             Connection connection = DriverManager.getConnection(
                     "jdbc:mysql://127.0.0.1:3306/vn",
                     "root",
-                    "po9wuono"
+                    "salis123"
             );
 
             Statement statement = connection.createStatement();
